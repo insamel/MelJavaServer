44ac0093e1fb6250a81f74f817b2c18e153586f9
Jes <kontaktjes@gmail.com>
2018-11-07 15:38:40 +0100

    lol added

---
+++ src/main/java/ClientTest.java
@@ -0,0 +1,10 @@
+public class ClientTest {
+    public static void main(String... args){
+		
+		Client test = new Client(localhost127.0.0.1); // localhost
+		while(true){  
+			test.startRunning();
+		}		
+		//clientSocket = serverSocket.accept();	// waits until client start up, and request connection on the host and port of the server. 
+    }
+}
