2937afe14879b8f23dbd22900d9ce3d589d1401f
insamel <44841783+insamel@users.noreply.github.com>
2018-11-18 10:19:40 +0100

    Create Blog.java

---
+++ src/main/java/data/Blog.java
@@ -0,0 +1,104 @@
+package data;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Blog implements Readable, Writable{
+    List<Post> tweets;
+
+    public Blog(){
+        tweets = new LinkedList<>();
+        try{
+            populateFromDisk();
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        finally {
+        	//TODO
+        }
+    }
+
+    public void addPost(Post newPost) {
+    	tweets.add(newPost);
+    }
+    
+    private void populateFromDisk() throws IOException {
+        List<Post> outputTweets = new LinkedList<Post>();
+        	  try {
+        	    FileInputStream fis = new  FileInputStream("new.txt");
+        	    ObjectInputStream ois = new ObjectInputStream(fis);
+        	    Object obj = ois.readObject();
+        	    outputTweets = (LinkedList<Post>) obj;
+        	  } 
+        	  catch (Exception e) {
+        	    System.out.println(e);
+        	  } 
+        	  tweets = outputTweets;
+        	
+    }
+
+
+//returns latest tweet
+    public Post readOne(){
+        return ((LinkedList<Post>) tweets).getLast();
+
+    }
+
+
+//returns all tweets
+    public List<Post> readAll() throws IOException {
+    	
+        List<Post> allTweets = new LinkedList<Post>();
+
+        for(Iterator<Post> it = tweets.iterator(); it.hasNext();){
+            allTweets.add(it.next());
+        }
+
+        if(readOne()!=null){
+            return allTweets; //return of list right?
+        }else{
+            return null;
+        }
+        
+    }
+
+//returns only E from the same author
+   public List<Post> readOwnPost() throws IOException{
+    List<Post> ownTweets = new LinkedList<Post>();
+/*
+        for(ListIterator<AbstractPost> it = tweets.ListIterator(); it.hasNext();){
+            AbstractPost post = it.next();
+
+            //if(post.getAuthor() == this.author???)
+            ownTweets.add(post);
+        }
+
+        if(ownTweets.readOne()!=null){*/
+            return ownTweets; //return of list right?
+       /* }else{
+            return null;
+        }
+        
+    }*/
+
+
+    }
+
+//save() rewrites the whole file, erasing previous data
+   public void save() throws IOException{
+		  try {
+		    FileOutputStream fos = new FileOutputStream ("new.txt");
+		    ObjectOutputStream oos = new ObjectOutputStream(fos);
+		    oos.writeObject(tweets);
+		    fos.close();
+		  } 
+		  catch (Exception e) {
+		    System.out.println(e);   
+		  }
+ }
+
+}
